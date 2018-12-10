48edad81db32be887c517026c09f217ffd28642d
Aula11 <aula11@imac-20.local>
2018-12-10 18:43:10 +0100

    interfaz Menu principal

--- firstAppWine/MenuController.swift
+++ firstAppWine/MenuController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MenuController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named:"fondo")!)
     }
 
     override func didReceiveMemoryWarning() {
