4afe559a7564e4d9a0c4b456fc561ac2820d5331
Aula11 <aula11@imac-20.local>
2018-12-17 19:19:41 +0100

    Avanzado enormemente el storyboard

---
+++ firstAppWine/RegistrarseController.swift
@@ -0,0 +1,34 @@
+//
+//  RegistrarseController.swift
+//  firstAppWine
+//
+//  Created by Aula11 on 17/12/18.
+//  Copyright © 2018 ual.es. All rights reserved.
+//
+
+import UIKit
+
+class RegistrarseController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named:"fondo")!)    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ firstAppWine/SeleccionarVinoController.swift
@@ -0,0 +1,35 @@
+//
+//  SeleccionarVinoController.swift
+//  firstAppWine
+//
+//  Created by Aula11 on 17/12/18.
+//  Copyright © 2018 ual.es. All rights reserved.
+//
+
+import UIKit
+
+class SeleccionarVinoController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+          self.view.backgroundColor = UIColor(patternImage: UIImage(named:"fondo")!)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
