0978b616d7660df87a7ebdda8965739d91551f53
Administrador <administrador@imac-23.local>
2019-01-04 00:35:59 +0100

    Intento de registro.

--- firstAppWine/RegistrarseController.swift
+++ firstAppWine/RegistrarseController.swift
@@ -7,9 +7,17 @@
 //
 
 import UIKit
+import CoreData
 
 class RegistrarseController: UIViewController {
 
+    @IBOutlet weak var nombreUser: UITextField!
+    @IBOutlet weak var contrasena: UITextField!
+    @IBOutlet weak var repContrasena: UITextField!
+    @IBOutlet weak var foto: UIImageView!
+    
+    var objeto = [NSManagedObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -30,5 +38,37 @@ class RegistrarseController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
+   
+    @IBAction func registrarse(sender: AnyObject) {
+        
+        registro(nombreUser.text!, contra: contrasena.text!)
+        
+    }
+    
+   
+    func registro(nombre: String, contra: String)
+    {
+        
+        print("Hola he llegado hasta aqui");
+        
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        let entity = NSEntityDescription.entityForName("Usuario", inManagedObjectContext: managedContext)
+        
+        let item = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        
+        item.setValue(nombre, forKey: "username")
+        item.setValue(contra, forKey: "password")
+        print(nombreUser.text!);
+        print(contrasena.text!);
+        
+        do{
+            try managedContext.save()
+            objeto.append(item as! Usuario)
+        } catch {
+            print ("Error")
+        }
+    }
 
 }
