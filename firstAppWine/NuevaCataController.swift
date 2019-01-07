385eef5b5e989703339cbe6c8d445341c439cdbf
Francisco Segovia <fran@Franciscos-iMac.local>
2019-01-07 21:45:17 +0100

    Slider dinamico

--- firstAppWine/NuevaCataController.swift
+++ firstAppWine/NuevaCataController.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
-class NuevaCataController: UIViewController {
+class NuevaCataController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var slider: UISlider!
     
+    @IBOutlet weak var picker: UIPickerView!
+    
     @IBOutlet weak var labelSlider: UILabel!
+    
+    var pickerData: [String] = [String]()
+    
+    var vino: Vino!
+    
+    var selectedValue = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+                pickerData = ["Alcalinity", "Alcohol", "Ash", "Color intensity", "Flavonoids", "Hue", "Non flavonoids Phenols", "Od280", "Proanthocyanins", "Proline"]
+        
+        self.picker.delegate = self
+        
+        self.picker.dataSource = self
+        
         labelSlider.text = "0"
 
         self.view.backgroundColor = UIColor(patternImage: UIImage(named:"fondo")!)
-
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,9 +42,79 @@ class NuevaCataController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //Number of colums of data
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    //The number of rows of data
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    //The data to return for the row and component (colum) that´s beign passed in
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    //Capture the picker view selection
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        selectedValue = pickerData[row]
+        //pickerData[picker.selectedRowInComponent(picker.selectedRowInComponent(row))]
+        
+         switch selectedValue{
+        case "Alcalinity":
+            sliderDinamico(5)
+           
+        case "Alcohol":
+            sliderDinamico(6)
+        
+         case "Ash":
+            sliderDinamico(7)
+            
+         case "Color intensity":
+            sliderDinamico(8)
+            
+         case "Flavonoids":
+            sliderDinamico(9)
+            
+         case "Hue":
+            sliderDinamico(10)
+            
+         case "Non flavonoids Phenols":
+            sliderDinamico(11)
+
+         case "Od280":
+            sliderDinamico(12)
+
+         case "Proanthocyanins":
+            sliderDinamico(13)
+
+         case "Proline":
+            sliderDinamico(14)
+
+
+
+
+
+        default:
+            print("ERROR")
+        }
+
+    }
+    
+    
+    
 
     @IBAction func valorSlider(sender: UISlider) {
         labelSlider.text = "\(slider.value)"
+    }
+    
+    func sliderDinamico(maximo : Float){
+        slider.value = 0
+        slider.maximumValue = maximo
+    
     }
     /*
     // MARK: - Navigation
