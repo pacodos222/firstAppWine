f1595d551eecbb61d66c41536e61c6035ae58ec8
Administrador <administrador@imac-23.local>
2019-01-08 17:59:46 +0100

    Subo registro, aún falta lo de la imagen que tengo que ver como hacerlo. He modificado la pantalla de registro para que oculte la contraseña a la vez que se escribe. Modificado la clase de registradocontroller para no permitir registrar un usuario con el mismo nombre ni con el campo contraseña y repetir contraseña diferentes.

--- firstAppWine/RegistrarseController.swift
+++ firstAppWine/RegistrarseController.swift
@@ -43,12 +43,12 @@ class RegistrarseController: UIViewController {
    
     @IBAction func registrarse(sender: AnyObject) {
         
-        registro(nombreUser.text!, contra: contrasena.text!)
+        registro(nombreUser.text!, contra: contrasena.text!,repC: repContrasena.text!)
         
     }
     
    
-    func registro(nombre: String, contra: String)
+    func registro(nombre: String, contra: String, repC: String)
     {
         
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
@@ -57,15 +57,54 @@ class RegistrarseController: UIViewController {
         
         let item = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
         
-        item.setValue(nombre, forKey: "username")
-        item.setValue(contra, forKey: "password")
+        if(listaUsers.contains(nombre)){
         
-        do{
-            try managedContext.save()
-            objeto.append(item as! Usuario)
-        } catch {
-            print ("Error")
+            let alerta = UIAlertController(title:"Error", message: "Ya existe un usuario con este nombre",
+                                           preferredStyle: UIAlertControllerStyle.Alert)
+            
+            let accion = UIAlertAction(title: "OK",
+                                       style: UIAlertActionStyle.Default) {
+                                        _ in alerta.dismissViewControllerAnimated(true, completion: nil)
+            }
+            
+            alerta.addAction(accion)
+            self.presentViewController(alerta, animated:true, completion: nil)
+    }
+    
+        
+        //Comprobamos que la repeticion de contraseña sea igual
+        if((contra == repC))
+        {
+            item.setValue(nombre, forKey: "username")
+            item.setValue(contra, forKey: "password")
+            
+            do{
+                try managedContext.save()
+                objeto.append(item as! Usuario)
+            } catch {
+                print ("Error")
+            }
         }
+        else
+        {
+            let alerta = UIAlertController(title:"Error", message: "No coinciden la contraseña",
+                                           preferredStyle: UIAlertControllerStyle.Alert)
+            
+            let accion = UIAlertAction(title: "OK",
+                                       style: UIAlertActionStyle.Default) {
+                                        _ in alerta.dismissViewControllerAnimated(true, completion: nil)
+            }
+            
+            alerta.addAction(accion)
+            self.presentViewController(alerta, animated:true, completion: nil)
+            self.contrasena.text = ""
+            self.repContrasena.text = ""
+        }
+        //Podemos poner una alerta avisando que la rep de contraseña ya está
+        //Tambien podemos poner otra alerta avisando de que el user ya está
+        
+        
+     
     }
     
     func mostrar() {
